cc4cf9a2325ce8310ff44e1f3a05f40cef2fbc9c
HominemSanctvm <gabriel.lucca@tutanota.com>
2022-02-08 19:46:03 -0300

    .

--- script.js
+++ script.js
@@ -1,20 +1,38 @@
 function createSquareGrids() {
-	const containerDiv = document.getElementById('container');
+	const containerDiv = document.getElementById('grid-container');
 
 	for (let i = 0; i < 256; i++) {
 		const div = document.createElement('div');
 		div.className = 'gridItems';
+		div.setAttribute('draggable', 'false'); 
 		containerDiv.appendChild(div);
 	}
 
 }
 createSquareGrids();
+let isDrawing = false;
 
 const gridItems = document.querySelectorAll('.gridItems');
+
 gridItems.forEach((element)=> {
-	element.addEventListener('click', function() {
-			element.style.backgroundColor = 'black';
-	});
+	element.addEventListener('mousedown', function() { 
+		isDrawing = true;
+	})
+});
+
+gridItems.forEach((element)=> {
+	element.addEventListener('mousemove', function() { 
+		if(isDrawing) {
+			paintTile(element);
+		}
+	})
+});
+
+gridItems.forEach((element)=> {
+	element.addEventListener('mouseup', function() { 
+		paintTile(element);
+		isDrawing = false;
+	})
 });
 
 const btnClear = document.getElementById('btnClear');
@@ -22,4 +40,17 @@ btnClear.addEventListener('click', function () {
 	gridItems.forEach((element) => {
 		element.style.backgroundColor = 'white'; 
 	})
+})
+
+function paintTile(element) {
+	element.style.backgroundColor = 'black';
+}
+
+const div = document.querySelector('div')
+div.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+
+div.addEventListener('drop', (e) => {
+  e.preventDefault()
 })
