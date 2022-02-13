e6e0ab219a8d25f8c9e1d10cbbf016bb9b5cb4ff
HominemSanctvm <gabriel.lucca@tutanota.com>
2022-02-12 22:44:13 -0300

    .

--- script.js
+++ script.js
@@ -4,15 +4,35 @@ function createSquareGrids() {
 	for (let i = 0; i < 256; i++) {
 		const div = document.createElement('div');
 		div.className = 'gridItems';
-		div.setAttribute('draggable', 'false'); 
+		div.setAttribute('draggable', false);
 		containerDiv.appendChild(div);
 	}
 
 }
-createSquareGrids();
-let isDrawing = false;
 
+function paintTile(element, color) {
+	element.style.backgroundColor = color;
+}
+
+createSquareGrids();
+
+let isDrawing = false;
+let colorPicked = '#000'
 const gridItems = document.querySelectorAll('.gridItems');
+const btnColorPicker = document.getElementById('btnColorPicker');
+const btnClear = document.getElementById('btnClear');
+
+gridItems.forEach((element)=> {
+	element.addEventListener('dragstart', (e) => {
+  		e.preventDefault()
+})	
+});
+
+gridItems.forEach((element)=> {
+	element.addEventListener('drop', (e) => {
+  		e.preventDefault()
+})
+});
 
 gridItems.forEach((element)=> {
 	element.addEventListener('mousedown', function() { 
@@ -23,34 +43,23 @@ gridItems.forEach((element)=> {
 gridItems.forEach((element)=> {
 	element.addEventListener('mousemove', function() { 
 		if(isDrawing) {
-			paintTile(element);
+			paintTile(element, colorPicked);
 		}
 	})
 });
 
 gridItems.forEach((element)=> {
 	element.addEventListener('mouseup', function() { 
-		paintTile(element);
+		paintTile(element, colorPicked);
 		isDrawing = false;
 	})
 });
 
-const btnClear = document.getElementById('btnClear');
+
 btnClear.addEventListener('click', function () {
 	gridItems.forEach((element) => {
 		element.style.backgroundColor = 'white'; 
 	})
 })
 
-function paintTile(element) {
-	element.style.backgroundColor = 'black';
-}
 
-const div = document.querySelector('div')
-div.addEventListener('dragstart', (e) => {
-  e.preventDefault()
-})
-
-div.addEventListener('drop', (e) => {
-  e.preventDefault()
-})
